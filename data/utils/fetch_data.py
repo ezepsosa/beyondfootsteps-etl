dc4c20c5cd1c5f3e268b38efd2e90e6565046bc2
ezepsosa <ezequielperez50@gmail.com>
2025-06-07 12:26:30 +0200

    Add logic to support multiples datasets from source UNHCR

--- data/utils/fetch_data.py
+++ data/utils/fetch_data.py
@@ -18,47 +18,17 @@ class fetch_data():
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.info("Obtaining json config")
-        self.config = load_config(r"C:\Users\EzequielPerez\Documents\Personal\Proyectos\MigrantScope\beyondfootsteps-etl\data\utils\config.json")
+        self.config = load_config(os.path.join(os.path.dirname(__file__),"config.json"))
     
-    def get_dataset_group(self, dataset_name):
-        solutions_datasets = {
-            "refugee_returnees", "idp_returnees", "refugee_naturalization",
-            "resettlement_submissions", "resettlement_departures",
-            "resettlement_arrivals", "resettlement_needs"
-        }
-        return "solutions" if dataset_name in solutions_datasets else "displacement"
-
-
     def build_unhcr_url(self, dataset_name):
         self.logger.info(f"Building URL for {dataset_name}")
         base_url = self.config['base_url']
-        year_from = self.config['year_range']['from']
-        year_to = self.config['year_range']['to']
-        
-        params = {
-            "data-finder": "on",
-            "data_finder[dataGroup]": self.get_dataset_group(dataset_name),
-            "data_finder[dataset]": dataset_name,
-            "data_finder[displayType]": "totals",
-            "data_finder[year__filterType]": "range",
-            "data_finder[year__rangeFrom]": year_from,
-            "data_finder[year__rangeTo]": year_to,
-            "data_finder[coo][displayType]": "displayAll",
-            "data_finder[coa][displayType]": "displayAll"
-        }
-        
-        population_types = self.config["datasets"].get(dataset_name, {}).get("population_types")
-        if population_types:
-            params["data_finder[populationType]"] = population_types
+        params = self.config['datasets'][dataset_name]['params']
         return f"{base_url}?{urlencode(params, doseq=True)}"
     
     def download_unhcr_url(self, url, output_dir, dataset, date):
         self.logger.info(f"Starting download")
-        headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "application/zip,application/octet-stream;q=0.9,*/*;q=0.8",
-        "Referer": "https://www.unhcr.org/refugee-statistics/download"
-        }
+        headers = self.config['headers']
 
         response = requests.get(url, headers=headers)
         
