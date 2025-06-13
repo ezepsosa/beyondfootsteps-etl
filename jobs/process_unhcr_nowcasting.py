4defced15aca4da80daecafce89dfeb461c9c0f8
ezepsosa <ezequielperez50@gmail.com>
2025-06-13 18:49:02 +0200

    Add silver layer of nowcasting

---
+++ jobs/process_unhcr_nowcasting.py
@@ -0,0 +1,24 @@
+from typing import override
+
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, concat_ws, coalesce, lit
+
+from sparklibs.job import SilverProcessor
+
+
+class ProcessUnhcrNowcasting(SilverProcessor):
+    def __init__(self):
+        super().__init__(origin="UNHCR", entity="nowcasting", id_column_name="id_nowcasting")
+
+    def _preprocess_dataframe(self, df: DataFrame) -> DataFrame:
+        return df.withColumn(
+            "id_nowcasting",
+            concat_ws(
+                "_",
+                coalesce(col("Year"), lit("")),
+                coalesce(col("Country of Origin ISO"), lit("")),
+                coalesce(col("Country of Asylum ISO"), lit(""))
+            )
+        )
+if __name__ == "__main__":
+    ProcessUnhcrNowcasting().execute()
