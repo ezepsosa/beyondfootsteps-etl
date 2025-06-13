43a4878653233a7477c3ab82df86d96602b0f39c
ezepsosa <ezequielperez50@gmail.com>
2025-06-13 19:06:38 +0200

    Add silver layer of resettlementdepartures

---
+++ jobs/process_unhcr_resettlementdepartures.py
@@ -0,0 +1,25 @@
+from typing import override
+
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, concat_ws, coalesce, lit
+
+from sparklibs.job import SilverProcessor
+
+
+class ProcessUnhcrResettlementDepartures(SilverProcessor):
+    def __init__(self):
+        super().__init__(origin="UNHCR", entity="resettlementdepartures", id_column_name="id_resettlementdepartures")
+
+    def _preprocess_dataframe(self, df: DataFrame) -> DataFrame:
+        return df.withColumn(
+            "id_resettlementdepartures",
+            concat_ws(
+                "_",
+                coalesce(col("Year"), lit("")),
+                coalesce(col("Country of Asylum ISO"), lit("")),
+                coalesce(col("Country of Origin ISO"), lit("")),
+                coalesce(col("Country of Resettlement ISO"), lit(""))
+            )
+        )
+if __name__ == "__main__":
+    ProcessUnhcrResettlementDepartures().execute()
