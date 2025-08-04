2dd2dfab11ed87f7bc61982a4c7255378cea1f4c
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-04 21:07:04 +0200

    Add column to asylum requests

--- jobs/create_asylum_requests.py
+++ jobs/create_asylum_requests.py
@@ -1,15 +1,19 @@
 from argparse import Namespace
 
-from pyspark.sql.functions import col, try_divide, lit
-
+from pyspark.sql.functions import (
+    col,
+    try_divide,
+    lit,
+    sum as _sum,
+    max as _max,
+    when,
+)
 from sparklibs.job import GoldJob, Configuration
 
 
 class AsylumRequestsJob(GoldJob):
     def run(self, configuration: Configuration, args: Namespace):
         kpi_name = "asylum_requests_kpi"
-
-        # DATAFRAMES LOAD
 
         df_asylumapplications = self._get_last_version_from_silver(
             configuration, origin="UNHCR", entity="asylumapplications"
@@ -18,10 +22,21 @@ class AsylumRequestsJob(GoldJob):
             configuration, origin="WorldBank", entity="worlddevelopmentindicators"
         )
 
-        # TABLES JOIN
-        
+        df_apps_agg = df_asylumapplications.groupBy(
+            "year",
+            "country_of_origin",
+            "country_of_origin_iso",
+            "country_of_asylum",
+            "country_of_asylum_iso",
+        ).agg(
+            _sum("applied").alias("applied"),
+            _max(when(col("app_pc") == "C", lit(True)).otherwise(lit(False))).alias(
+                "app_pc"
+            ),
+        )
+
         df_asylum_requests = (
-            df_asylumapplications.alias("applications")
+            df_apps_agg.alias("applications")
             .join(
                 df_worldpopulation.alias("population"),
                 on=[
@@ -32,30 +47,26 @@ class AsylumRequestsJob(GoldJob):
                 how="left",
             )
             .select(
-                col("applications.id_asylumapplications"),
                 col("applications.year"),
                 col("applications.country_of_origin"),
                 col("applications.country_of_origin_iso"),
                 col("applications.country_of_asylum"),
                 col("applications.country_of_asylum_iso"),
                 col("applications.applied"),
+                col("applications.app_pc"),
                 col("population.population"),
             )
             .withColumn(
                 "applied_per_100k",
                 try_divide(col("applications.applied"), col("population.population"))
-                * lit(100000),
+                * lit(100_000),
             )
-            .withColumnRenamed("id_asylumapplications", "id")
-            
         )
 
         output_directory = f"{configuration.__getattribute__('output_dir')}/{kpi_name}"
-
         df_asylum_requests.write.parquet(output_directory, mode="overwrite")
-
         self._save_in_database(df_asylum_requests, kpi_name, configuration)
 
 
 if __name__ == "__main__":
-    (AsylumRequestsJob().execute(),)
+    AsylumRequestsJob().execute()
