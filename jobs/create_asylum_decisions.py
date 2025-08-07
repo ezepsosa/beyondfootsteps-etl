38efeb50666ee63e7f1b5daaeb26039ad46a3e57
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-07 20:03:38 +0200

    UPDATE: Refactor asylum decisions job to include aggregation and acceptance rate calculation

--- jobs/create_asylum_decisions.py
+++ jobs/create_asylum_decisions.py
@@ -1,6 +1,14 @@
 from argparse import Namespace
 
-from pyspark.sql.functions import col, try_divide
+from pyspark.sql.functions import (
+    col,
+    try_divide,
+    sum as _sum,
+    max as _max,
+    when,
+    lit,
+    concat_ws,
+)
 
 from sparklibs.job import GoldJob, Configuration
 
@@ -9,38 +17,43 @@ class AsylumDecisionsJobs(GoldJob):
     def run(self, configuration: Configuration, args: Namespace):
         kpi_name = "asylum_decisions_kpi"
 
-        # DATAFRAMES LOAD
-
         df_asylumdecisions = self._get_last_version_from_silver(
             configuration, origin="UNHCR", entity="asylumdecisions"
         )
 
-        # TABLES JOIN
+        df_decisions_agg = df_asylumdecisions.groupBy(
+            "year",
+            "country_of_origin",
+            "country_of_origin_iso",
+            "country_of_asylum",
+            "country_of_asylum_iso",
+        ).agg(
+            _sum("dec_recognized").alias("dec_recognized"),
+            _sum("dec_other").alias("dec_other"),
+            _sum("dec_rejected").alias("dec_rejected"),
+            _sum("dec_closed").alias("dec_closed"),
+            _sum("dec_total").alias("dec_total"),
+            _max(when(col("dec_pc") == "C", lit(True)).otherwise(lit(False))).alias(
+                "dec_pc"
+            ),
+        )
 
-        df_asylumdecisions = df_asylumdecisions.withColumn(
+        df_asylum_decisions = df_decisions_agg.withColumn(
             "acceptance_rate", try_divide(col("dec_recognized"), col("dec_total"))
-        ).select(
-            col("id_asylumdecisions"),
-            col("year"),
-            col("country_of_origin"),
-            col("country_of_origin_iso"),
-            col("country_of_asylum"),
-            col("country_of_asylum_iso"),
-            col("dec_recognized"),
-            col("dec_other"),
-            col("dec_rejected"),
-            col("dec_closed"),
-            col("dec_total"),
-            col("acceptance_rate"),
-            col("intake_date"),
-        ).withColumnRenamed("id_asylumdecisions", "id")
+        ).withColumn(
+            "id",
+            concat_ws(
+                "_",
+                col("year"),
+                col("country_of_origin_iso"),
+                col("country_of_asylum_iso"),
+            ),
+        )
 
         output_directory = f"{configuration.__getattribute__('output_dir')}/{kpi_name}"
-
-        df_asylumdecisions.write.parquet(output_directory, mode="overwrite")
-
-        self._save_in_database(df_asylumdecisions, kpi_name, configuration)
+        df_asylum_decisions.write.parquet(output_directory, mode="overwrite")
+        self._save_in_database(df_asylum_decisions, kpi_name, configuration)
 
 
 if __name__ == "__main__":
-    (AsylumDecisionsJobs().execute(),)
+    AsylumDecisionsJobs().execute()
